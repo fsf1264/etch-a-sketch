70d5730952ef6824af752651fcc16c124925e0cf
fsf1264 <142989915+fsf1264@users.noreply.github.com>
2025-10-21 23:13:42 +0300

    set up grids and style them

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,2 +1,12 @@
 const container = document.querySelector('.container')
-
+let grids = []
+for (let i = 0; i < 16; i++){
+    for (let i = 0; i < 16; i++){
+    let newElement 
+    grids.push(newElement)
+}
+}
+for (let grid of grids){
+grid = document.createElement('div')
+container.appendChild(grid)
+}

f07f28defa664bfe44fb30abba9ca0bb9d5ce9b2
fsf1264 <142989915+fsf1264@users.noreply.github.com>
2025-10-22 23:42:16 +0300

    add functionality to the grids button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,17 @@
-function setupGrids(numberOfGrids = 16){
+function setupGrids(numberOfGrids = 16, containerWidth = 864, containerHeight = 864){
+const containerGrids = document.querySelectorAll('.container > div')
+if(containerGrids!= []){   
+    for(let grid of containerGrids){
+        container.removeChild(grid)
+    }
+}
+let filteredNumberOfGrids = numberOfGrids
+if(numberOfGrids > 100){
+    filteredNumberOfGrids = 100
+}
 let grids = []
-for (let i = 0; i < numberOfGrids; i++){
-    for (let i = 0; i < numberOfGrids; i++){
+for (let i = 0; i < filteredNumberOfGrids; i++){
+    for (let i = 0; i < filteredNumberOfGrids; i++){
     let newElement 
     grids.push(newElement)
 }
@@ -9,6 +19,13 @@ for (let i = 0; i < numberOfGrids; i++){
 for (let grid of grids){
 grid = document.createElement('div')
 grid.classList.add('grids')
+let gridWidth = containerWidth / filteredNumberOfGrids
+let gridHeight = containerHeight / filteredNumberOfGrids
+grid.style.width = `${gridWidth}px`
+grid.style.height = `${gridHeight}px`
+if (gridWidth < 25){
+    grid.style.border = 'solid 0px black'
+}
 container.appendChild(grid)
 }}
 
@@ -19,8 +36,21 @@ function listentoMouseenter(grids){
 }
 }
 
+function listentoGridsButton(gridsButton){
+    gridsButton.addEventListener("click", 
+    () =>{
+    let numberOfGrids = prompt('What number of squares per side do you want?')
+    setupGrids(numberOfGrids)
+    let grids = document.querySelectorAll('.grids')
+    listentoMouseenter(grids)
+    }) 
+}
+
 const container = document.querySelector('.container')
+const btnGrids = document.querySelector('.btn-grids')
 setupGrids()
-const grids = document.querySelectorAll('.grids')
+let grids = document.querySelectorAll('.grids')
+listentoGridsButton(btnGrids)
 listentoMouseenter(grids)
+
 

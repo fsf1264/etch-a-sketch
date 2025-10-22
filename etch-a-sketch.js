c890dfd837f6573d2ae52e9ff7c9c1a23e74feec
fsf1264 <142989915+fsf1264@users.noreply.github.com>
2025-10-22 21:57:13 +0300

    change size of grids,add event listeners for hover

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,26 @@
-const container = document.querySelector('.container')
+function setupGrids(numberOfGrids = 16){
 let grids = []
-for (let i = 0; i < 16; i++){
-    for (let i = 0; i < 16; i++){
+for (let i = 0; i < numberOfGrids; i++){
+    for (let i = 0; i < numberOfGrids; i++){
     let newElement 
     grids.push(newElement)
 }
 }
 for (let grid of grids){
 grid = document.createElement('div')
+grid.classList.add('grids')
 container.appendChild(grid)
+}}
+
+function mouseenterListener(grids){
+    for(let grid of grids){
+    grid.addEventListener("mouseenter", () =>{
+    grid.style.backgroundColor = 'blue'})    
 }
+}
+
+const container = document.querySelector('.container')
+setupGrids()
+const grids = document.querySelectorAll('.grids')
+mouseenterListener(grids)
+
